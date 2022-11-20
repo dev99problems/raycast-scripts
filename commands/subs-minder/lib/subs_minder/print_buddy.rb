a15007339ee2a1bd8ec2469cd528858966d730ce
Eugene Chulkov <your_email>
2022-11-20 22:21:19 +0000

    [subsminder] add paid and next payment indication

--- commands/subs-minder/lib/subs_minder/print_buddy.rb
+++ commands/subs-minder/lib/subs_minder/print_buddy.rb
@@ -22,10 +22,44 @@ class String
   end
 end
 
+ROW_LEN = 50
+EMOJI_LEN = 2
+
+class Row # :nodoc:
+  include Enumerable
+
+  attr_reader :data
+
+  def initialize(sub)
+    @data = {}
+
+    @data['name'] = sub['name']
+    @data['price'] = sub['price']
+    @data['valid_to'] = sub['valid_through']
+    @data['valid_to_formatted'] = convert_date(sub['valid_through'])
+    @data['is_paid'] = Date.parse(@data['valid_to']) <= DateTime.now
+  end
+
+  def convert_date(date)
+    date = Date.parse(date)
+    date.strftime('%d %b')
+  end
+
+  def to_s
+    valid_output = "#{@data['valid_to_formatted']} "
+    price_output = format('%0.1f$', @data['price'])
+    name_output = data['name'].ljust(ROW_LEN - EMOJI_LEN - price_output.length - valid_output.length)
+
+    valid_output.green + name_output.light_blue + price_output.green
+  end
+
+  def <=>(other)
+    other.data['valid_to_formatted'] <=> data['valid_to_formatted']
+  end
+end
+
 class PrintBuddy # :nodoc:
   attr_reader :total
-
-  ROW_LEN = 50
 
   def initialize
     @total = 0
@@ -35,29 +69,38 @@ class PrintBuddy # :nodoc:
     puts "#{'-' * ROW_LEN}".light_blue
   end
 
-  def convert_date(date)
-    date = Date.parse(date)
-    date.strftime('%d %b')
+  def table_rows(active_subs)
+    rows = active_subs.map do |sub|
+      row = Row.new(sub)
+      @total += row.data['price']
+      row
+    end
+  end
+
+  def wrap_with_emojis(rows)
+    recent_paid_sub_idx = rows.index { |row| row.data['is_paid'] == true } || 0
+    next_payment_idx = [recent_paid_sub_idx - 1, 0].max
+
+    rows.map.with_index do |row, idx|
+      if row.data['is_paid']
+        "✅#{row}"
+      elsif next_payment_idx == idx
+        "➡️#{row}"
+      else
+        ' ' * EMOJI_LEN + row.to_s
+      end
+    end
   end
 
   def print_subs_table(active_subs)
-    rows = active_subs.map do |sub|
-      name = sub['name']
-      price = sub['price']
-      valid_through = convert_date(sub['valid_through'])
-      @total += price
+    rows = table_rows(active_subs)
+    table_data = wrap_with_emojis(rows.sort)
 
-      valid_output = "#{valid_through} "
-      price_output = format('%0.2f💲', price)
-      name_output = name.ljust(ROW_LEN - price_output.length - valid_output.length)
-      row = valid_output.green + name_output.light_blue + price_output.green
-    end
-
-    rows.sort.each { |row| puts row}
+    table_data.each { |row| puts row }
   end
 
   def print_total
-    puts(' '.ljust(ROW_LEN - (@total.to_s.length + 1)) << "#{@total}💲".green)
+    puts(' '.ljust(ROW_LEN - (@total.to_s.length + 1)) << "#{@total}$".green)
   end
 
   def print_month_payout(active_subs)
