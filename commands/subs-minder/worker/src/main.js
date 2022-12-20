d3ebbce598207218d4e5b1e5b9c9cfee1f1f2f7b
Eugene Chulkov <genechulkov@gmail.com>
2022-12-20 11:04:13 +0000

    move dev scripts to dev/

--- commands/subs-minder/worker/src/main.js
+++
@@ -1,8 +0,0 @@
-import airtable from './airtable.js'
-
-const { RecordsUpdater, env: { BASE, TABLE, KEY } } = airtable
-const subs_updater = new RecordsUpdater({ base: BASE, table: TABLE, key: KEY })
-
-!(async () => {
-  await subs_updater.main()
-})()
