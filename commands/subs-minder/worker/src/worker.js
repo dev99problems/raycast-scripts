23b77f480c33b5ebf6a709e10215deda5ff5d8eb
Eugene Chulkov <genechulkov@gmail.com>
2022-12-20 16:22:42 +0000

    update worker.js to support sending messages to telegram on cron

--- commands/subs-minder/worker/src/worker.js
+++ commands/subs-minder/worker/src/worker.js
@@ -1,27 +1,34 @@
 import airtable from './airtable.js'
+import { Sender } from './sender.js'
+import { log } from './utils.js'
 
-const { RecordsUpdater, env: { BASE, TABLE, KEY }} = airtable
+const { RecordsUpdater, env: { BASE, TABLE, KEY, TG_AUTH_KEY }} = airtable
 const subs_updater = new RecordsUpdater({ base: BASE, table: TABLE, key: KEY })
+const sender = new Sender(TG_AUTH_KEY)
 
-async function handleFetchResponse(req) {
+async function handleFetchResponse(req, env) {
   const url = new URL(req.url)
 
   if (url.pathname === '/') {
-    await subs_updater.main()
+    const {active_subs, past_subs} = await subs_updater.split_subs()
+    // await subs_updater.main({active_subs, past_subs})
+    await sender.send_reminder(active_subs, env)
 
-    return new Response('If this is not a success, then what?')
+    return new Response('JMSG: Success request to /')
   }
 
-  return new Response()
+  return new Response('JMSG: Success mock response')
 }
 
 export default {
   // NOTE: Usefull for development
-  // async fetch(req, env, ctx) {
-  //   return handleFetchResponse(req)
+  // async fetch(req, env, ctx) {    
+  //   return handleFetchResponse(req, env)
   // },
-  async scheduled(controller, env, ctx) {
-    console.log('cron processed at ', new Date())
-    await subs_updater.main()
+  async scheduled(event, env, ctx) {
+    log('cron processed at ', new Date())
+    const {active_subs, past_subs} = await subs_updater.split_subs()
+    await subs_updater.main({active_subs, past_subs})
+    await sender.send_reminder(active_subs, env)
   }
 }
