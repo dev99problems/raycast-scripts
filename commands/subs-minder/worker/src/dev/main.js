9d988a700d695506272370a36129aaef7f8fc7b2
Eugene Chulkov <genechulkov@gmail.com>
2022-12-20 12:59:49 +0000

    add data.json back again lol

--- commands/subs-minder/worker/src/dev/main.js
+++ commands/subs-minder/worker/src/dev/main.js
@@ -1,8 +1,11 @@
 import airtable from '../airtable.js'
+import { Sender } from '../sender.js'
 
 const { RecordsUpdater, env: { BASE, TABLE, KEY, TG_AUTH_KEY } } = airtable
 const subs_updater = new RecordsUpdater({ base: BASE, table: TABLE, key: KEY })
+const sender = new Sender(TG_AUTH_KEY)
 
 !(async function() {
-    await subs_updater.main()
+    // await subs_updater.main()
+    await sender.send_notification('hey there')
 })()
