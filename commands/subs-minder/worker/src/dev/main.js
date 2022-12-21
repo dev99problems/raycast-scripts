b9d8137bbb303bd87423ec1df2f517b59ca7dea8
Eugene Chulkov <genechulkov@gmail.com>
2022-12-21 14:09:39 +0000

    [subsminder] indent. alighment in dev/

--- commands/subs-minder/worker/src/dev/main.js
+++ commands/subs-minder/worker/src/dev/main.js
@@ -6,7 +6,7 @@ const subs_updater = new RecordsUpdater({ base: BASE, table: TABLE, key: KEY })
 const sender = new Sender(TG_AUTH_KEY)
 
 !(async function() {
-    const {active_subs, past_subs} = await subs_updater.split_subs()
-    // await subs_updater.main({active_subs, past_subs})
-    await sender.send_reminder(active_subs)
+  const { active_subs, past_subs } = await subs_updater.split_subs()
+  // await subs_updater.main({active_subs, past_subs})
+  await sender.send_reminder(active_subs)
 })()
