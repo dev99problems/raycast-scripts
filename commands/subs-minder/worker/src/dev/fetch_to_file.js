eee20ba8eb2f1684ac951f840385d599e6684e72
Gene Chulkov <genechulkov@gmail.com>
2022-12-21 14:12:59 +0000

    avoid empty messages sending (#33)
    * [subsminder] fix avoiding of empty messages sent
    
    * [subsminder] indent. alighment in dev/

--- commands/subs-minder/worker/src/dev/fetch_to_file.js
+++ commands/subs-minder/worker/src/dev/fetch_to_file.js
@@ -4,7 +4,7 @@ import airtable from '../airtable.js'
 const { Airtable, env: { BASE, TABLE, KEY } } = airtable
 const airt = new Airtable({ base: BASE, table: TABLE, key: KEY })
 
-!(async() => {
+!(async () => {
   const records = await airt.get_records()
   fs.writeFileSync('./src/dev/data.json', JSON.stringify(records))
 })()
